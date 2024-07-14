dcd148a086a92fde75ae21ae6d2f883d8f1834fb
Raulitoflying <1017329098@qq.com>
2024-07-14 16:53:49 -0700

    Add the Style module

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import Courses from "./Courses";
 import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
+import "./styles.css";
 
 
 export default function Kanbas() {
