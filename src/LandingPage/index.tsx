23a92fb1a9eed54cb10abff36f764cf1fab152d0
Raulitoflying <1017329098@qq.com>
2024-07-15 02:51:37 -0700

    Updated the LandingPage section

--- src/LandingPage/index.tsx
+++ src/LandingPage/index.tsx
@@ -6,8 +6,8 @@ const LandingPage: React.FC = () => {
     <div>
       <h1>Landing Page</h1>
       <br></br>
-      <h2>Welcome to CS5610 Web Development </h2>
-      <p><strong>Name:</strong> Xinyi Wang/Sophia</p>
+      <h2>Welcome to CS5610/Bienvenue sur CS5610</h2>
+      <p><strong>Name:</strong> Yixiang Zhou/Raul</p>
       <p><strong>Section:</strong> CS5610 Summer II</p>
 
       <h2>Labs</h2>
@@ -23,7 +23,7 @@ const LandingPage: React.FC = () => {
       </ul>
 
       <h2>Links to all relevant source code repositories</h2>
-      <p><a href="https://github.com/thisissophiawang/kanbas-react-web-app/tree/a1">Github Repository</a></p>
+      <p><a href="https://github.com/Raulitoflying/kanbas-react-web-app/tree/a2">Github Repository</a></p>
 
     </div>
   );
