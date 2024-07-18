2bec7d18a7b95328676af4d66b926d9b28a86905
Raulitoflying <1017329098@qq.com>
2024-07-17 19:19:39 -0700

    Update CourseStatus component and add Status.css

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,6 +1,5 @@
 export default function Dashboard() {
   return (
-    
     <div>
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
       <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
