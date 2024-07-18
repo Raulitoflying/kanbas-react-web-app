35bcecc143a10c340dd74d1ecba1da9fbbe1fe4d
Raulitoflying <1017329098@qq.com>
2024-07-17 19:21:06 -0700

    Add Kanbas component and update routing

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -2,14 +2,11 @@ import { Navigate, Route, Routes } from "react-router";
 import Lab1 from "./Lab1";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
-import KanbasNavigation from '../Kanbas';
 import TOC from "./TOC";
-
 
 export default function Labs() {
   return (
     <div>
-      <KanbasNavigation />
       <h1>Yixiang Zhou/Raul</h1>
       <h1>Labs</h1>
       <TOC />
@@ -19,7 +16,7 @@ export default function Labs() {
         <Route path="/Lab2" element={<Lab2 />} />
         <Route path="/Lab3" element={<Lab3 />} />
       </Routes>
-      <a id="wd-github" href="https://github.com/Raulitoflying/kanbas-react-web-app/tree/a1">
+      <a href="https://github.com/Raulitoflying/kanbas-react-web-app/tree/a1/">
         GitHub
       </a>
     </div>
