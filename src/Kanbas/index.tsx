7f081ddc7c6792e676a37813e0006411360f1732
Raulitoflying <1017329098@qq.com>
2024-07-22 18:23:26 -0700

    Updated the react-router-dom component

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,24 +1,28 @@
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import Courses from "./Courses";
 import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
+import "./styles.css";
+import React from "react";
 
 export default function Kanbas() {
   return (
-    <div id="wd-kanbas">
-      <table border={1} width="100%">
-        <tr>
-          <td valign="top">
-            <KanbasNavigation />
-          </td>
-          <td valign="top">
-            <Routes>
-              <Route path="/Courses/:id/*" element={<Courses />} />
-              <Route path="/Dashboard" element={<Dashboard />} />
-            </Routes>
-          </td>
-        </tr>
-      </table>
+    <div id="wd-kanbas" className="h-100">
+      <div className="d-flex h-100">
+        <div className="d-none d-md-block bg-black">
+          <KanbasNavigation />
+        </div>
+        <div className="flex-fill p-4">
+          <Routes>
+            <Route path="/" element={<Navigate to="Dashboard" />} />
+            <Route path="Account" element={<h1>Account</h1>} />
+            <Route path="Dashboard" element={<Dashboard />} />
+            <Route path="Courses/:cid/*" element={<Courses />} />
+            <Route path="Calendar" element={<h1>Calendar</h1>} />
+            <Route path="Inbox" element={<h1>Inbox</h1>} />
+          </Routes>
+        </div>
+      </div>
     </div>
-  );
-}
+);}
+
