98ead921afb6d47a1b2a4dca19ba93e3da1014a5
Raulitoflying <1017329098@qq.com>
2024-07-22 18:41:43 -0700

    Same issue with a2

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,86 @@
+import React from 'react';
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import BooleanVariables from "./BooleanVariables";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import LegacyFunctions from "./LegacyFunctions";
+import ArrowFunctions from "./ArrowFunctions";
+import ImpliedReturn from "./ImpliedReturn";
+import TemplateLiterals from "./TemplateLiterals";
+import SimpleArrays from "./SimpleArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ForLoops from "./ForLoops";
+import MapFunction from "./MapFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import FilterFunction from "./FilterFunction";
+import JsonStringify from "./JsonStringify";
+import House from "./House";
+import TodoItem from "./TodoItem";
+import TodoList from "./todos/TodoList";
+import Spreading from "./todos/Spreading";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
+import MathComponent from "./MathComponent";
+import DestructingImports from "./DestructingImports";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import Add from "./Add";
+import Square from "./Square";
+import Highlight from "./Highlight";
+import AddPathParameters from "./AddPathParameters";
+import PathParameters from "./PathParameters";
+
 export default function Lab3() {
+  console.log("Hello World!");
+
   return (
-    <div>
-      <h2>Lab 3</h2>
+    <div id="wd-lab3" className="container-fluid">
+      <h3>Lab 3</h3>
+      <VariablesAndConstants />
+      <VariableTypes />
+      <BooleanVariables />
+      <IfElse />
+      <TernaryOperator />
+      <ConditionalOutputInline />
+      <ConditionalOutputIfElse />
+      <LegacyFunctions />
+      <ArrowFunctions />
+      <ImpliedReturn />
+      <TemplateLiterals />
+      <SimpleArrays />
+      <ArrayIndexAndLength />
+      <AddingAndRemovingToFromArrays />
+      <ForLoops />
+      <MapFunction />
+      <FindFunction />
+      <FindIndex />
+      <FilterFunction />
+      <JsonStringify />
+      <House />
+      <TodoItem />
+      <TodoList />
+      <Spreading />
+      <Destructing />
+      <FunctionDestructing />
+      <MathComponent />
+      <DestructingImports />
+      <Classes />
+      <Styles />
+      <Add a={3} b={5} />
+      <Square children={4} />
+      <Highlight>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo voluptas
+        non delectus doloribus quod vel iure, accusamus reprehenderit dolorem
+        impedit et sapiente adipisci perferendis fugiat libero ratione?
+        Molestias, maiores sed!
+      </Highlight>
+      <AddPathParameters />
+      <PathParameters />
     </div>
   );
 }
---
+++ src/LandingPage/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LandingPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Landing Page</h1>
+      <br></br>
+      <h2>Welcome to CS5610 Web Development </h2>
+      <p><strong>Name:</strong> Yixiang Zhou/Raul</p>
+      <p><strong>Section:</strong> CS5610 Summer II</p>
+
+      <h2>Labs</h2>
+      <ul>
+        <li><Link to="/Labs/Lab1">Lab 1</Link></li>
+        <li><Link to="/Labs/Lab2">Lab 2</Link></li>
+        <li><Link to="/Labs/Lab3">Lab 3</Link></li>
+      </ul>
+
+      <h2>Kanbas</h2>
+      <ul>
+        <li><Link to="/Kanbas">Kanbas</Link></li>
+      </ul>
+
+      <h2>Links to all relevant source code repositories</h2>
+      <p><a href="https://github.com/Raulitoflying/kanbas-react-web-app/tree/a1/">Github Repository</a></p>
+
+    </div>
+  );
+}
+
+export default LandingPage;
