78ce83b8470440507796d1e0c6223ad838ab0daf
Raulitoflying <1017329098@qq.com>
2024-07-22 18:22:32 -0700

    Updated the Labs component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import React from "react";
-import Labs from "./Labs";
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import Kanbas from "./Kanbas";
+import LandingPage from "./LandingPage";
+import Labs from "./Labs";
+import Courses from "./Kanbas/Courses"; // Import Courses component
 
 function App() {
   return (
     <HashRouter>
       <div>
         <Routes>
-          <Route path="/" element={<Navigate to="Labs" />} />
-          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/" element={<Navigate to="/LandingPage" />} />
           <Route path="/Kanbas/*" element={<Kanbas />} />
+          <Route path="/LandingPage" element={<LandingPage />} />
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/Courses/:cid/*" element={<Courses />} /> {/* Include course ID in the route */}
         </Routes>
+        <a href="https://github.com/Raulitoflying/kanbas-react-web-app/tree/a1/">Github</a>
       </div>
     </HashRouter>
   );
