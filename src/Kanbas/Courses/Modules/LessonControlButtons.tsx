391dd7b738f75ade476ec300617183890ed96f3a
Raulitoflying <1017329098@qq.com>
2024-07-22 18:31:26 -0700

    Edit the modules with green check mark.

---
+++ src/Kanbas/Courses/Modules/LessonControlButtons.tsx
@@ -0,0 +1,12 @@
+import { BsPlus } from 'react-icons/bs';
+import GreenCheckmark from './GreenCheckmark';
+import React from 'react';
+
+export default function ModuleControlButtons() {
+  return (
+    <div className="float-end">
+      <GreenCheckmark />
+      <BsPlus className="fs-4" />
+    </div>
+  );
+}
