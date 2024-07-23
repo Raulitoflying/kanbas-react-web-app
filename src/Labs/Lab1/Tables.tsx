a30dbba499316d3511a496fc764b6c7959716963
Raulitoflying <1017329098@qq.com>
2024-07-22 18:41:24 -0700

    Same issue with a2

--- src/Labs/Lab1/Tables.tsx
+++ src/Labs/Lab1/Tables.tsx
@@ -1,3 +1,5 @@
+
+
 export default function Tables() {
   return (
     <div id="wd-tables">
@@ -5,7 +7,7 @@ export default function Tables() {
       <table border={1} width="100%">
         <thead>
           <tr>
-            <th>Quiz</th> {/* table heading*/}
+            <th>Quiz</th>
             <th>Topic</th>
             <th>Date</th>
             <th>Grade</th>
@@ -13,28 +15,28 @@ export default function Tables() {
         </thead>
         <tbody>
           <tr>
-            <td>Q1</td> {/* table data / cell */}
+            <td>Q1</td>
             <td>HTML</td>
             <td>2/3/21</td>
             <td>85</td>
           </tr>
           <tr>
-            <td>Q2</td> {/* table data / cell */}
+            <td>Q2</td>
             <td>CSS</td>
-            <td>2/3/21</td>
-            <td>85</td>
+            <td>2/10/21</td>
+            <td>90</td>
           </tr>
           <tr>
-            <td>Q3</td> {/* table data / cell */}
+            <td>Q3</td>
             <td>JavaScript</td>
-            <td>2/3/21</td>
-            <td>85</td>
+            <td>2/17/21</td>
+            <td>95</td>
           </tr>
         </tbody>
         <tfoot>
           <tr>
-            <td colSpan={3}>Average</td> {/* column span */}
-            <td>85</td>
+            <td colSpan={3}>Average</td>
+            <td>90</td>
           </tr>
         </tfoot>
       </table>
