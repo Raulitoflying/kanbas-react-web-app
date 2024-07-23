951316e63b520a0d206390e0885ccde1f1c0d5d7
Raulitoflying <1017329098@qq.com>
2024-07-22 18:38:38 -0700

    Upload TOC

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,31 +1,42 @@
+import React from "react";
+import { useLocation } from "react-router";
 export default function TOC() {
-  return (
-    <ul>
-      <li>
-        <a id="wd-a" href="#/Labs">
-          Labs
-        </a>
-      </li>
-      <li>
-        <a id="wd-a1" href="#/Labs/Lab1">
-          Lab 1
-        </a>
-      </li>
-      <li>
-        <a id="wd-a2" href="#/Labs/Lab2">
-          Lab 2
-        </a>
-      </li>
-      <li>
-        <a id="wd-a3" href="#/Labs/Lab3">
-          Lab 3
-        </a>
-      </li>
-      <li>
-        <a id="wd-a3" href="#/Kanbas">
-          Kanbas
-        </a>
-      </li>
-    </ul>
-  );
+    const { pathname } = useLocation();
+    return (
+        <ul className="nav nav-pills" id="wd-toc">
+            <li className="nav-item">
+                <a id="wd-a" href="#/Labs" className="nav-link">
+                    Labs
+                </a>
+            </li>
+            <li className="nav-item">
+                <a id="wd-a1" href="#/Labs/Lab1"
+                   className={`nav-link ${pathname.includes("/Labs/Lab1") ? "active" : ""}`}>
+                    Lab 1
+                </a>
+            </li>
+            <li className="nav-item">
+                <a id="wd-a2" href="#/Labs/Lab2"
+                   className={`nav-link ${pathname.includes("/Labs/Lab2") ? "active" : ""}`}>
+                    Lab 2
+                </a>
+            </li>
+            <li className="nav-item">
+                <a id="wd-a3" href="#/Labs/Lab3"
+                   className={`nav-link ${pathname.includes("/Labs/Lab3") ? "active" : ""}`}>
+                    Lab 3
+                </a>
+            </li>
+            <li className="nav-item">
+                <a id="wd-k" href="#/Kanbas" className="nav-link">
+                    Kanbas
+                </a>
+            </li>
+            <li className="nav-item">
+                <a id="wd-github" href="https://github.com/Raulitoflying" className="nav-link" target="_blank">
+                    My GitHub
+                </a>
+            </li>
+        </ul>
+    );
 }
