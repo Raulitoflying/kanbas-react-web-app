7730eb5acafef8dd3e3305de0b5e8bbb82d36d3f
Raulitoflying <1017329098@qq.com>
2024-07-22 18:24:06 -0700

    Updated the data-drive component

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,38 +1,51 @@
-import { Routes, Route, Navigate } from "react-router";
+import { Navigate, Route, Routes, useParams, useLocation } from "react-router";
 import CoursesNavigation from "./Navigation";
+import KanbasNavigation from "../Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
+import Grades from "./Grades/Grades";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { courses } from "../Database";
+import { FaAlignJustify } from 'react-icons/fa6';
+import React from "react";
+
 
 export default function Courses() {
+  const { cid } = useParams();
+  const course = courses.find((course) => course._id === cid);
+  const { pathname } = useLocation();
+  
   return (
-      <div id="wd-courses">
-        <h2>Course 1234</h2>
-        <hr />
-        <table width="100%">
-          <tbody>
-          <tr>
-            <td valign="top">
-              <CoursesNavigation />
-            </td>
-            <td valign="top">
-              <Routes>
-                <Route path="/" element={<Navigate to="Home" />} />
-                <Route path="/Home" element={<Home />} />
-                <Route path="/Modules" element={<Modules />} />
-                <Route path="/Piazza" element={<h1>Piazza</h1>} />
-                <Route path="/Zoom" element={<h1>Zoom</h1>} />
-                <Route path="Assignments" element={<Assignments />} />
-                <Route path="Assignments/:id" element={<AssignmentEditor />} />
-                <Route path="/Quizzes" element={<h1>Quizzes</h1>} />
-                <Route path="/Grades" element={<h1>Grades</h1>} />
-              </Routes>
-            </td>
-          </tr>
-          </tbody>
-        </table>
+    <div className="d-flex h-100">
+      <div className="d-none d-md-block bg-black">
+        <KanbasNavigation />
       </div>
+      <div id="wd-courses" className="flex-fill">
+        <h2 className="text-danger">
+          <FaAlignJustify className="me-3 fs-4 mb-1" />
+          {course && course.name} &gt; {pathname.split("/")[4]}
+        </h2>
+        <div className="d-flex h-100">
+          <div className="d-none d-md-block bg-light"> {/* Change bg-black to bg-light */}
+            <CoursesNavigation />
+          </div>
+          <div className="flex-fill p-4">
+            <Routes>
+              <Route path="/" element={<Navigate to="Home" />} />
+              <Route path="Home" element={<Home />} />
+              <Route path="Modules" element={<Modules />} />
+              <Route path="Piazza" element={<h1>Piazza</h1>} />
+              <Route path="Zoom" element={<h1>Zoom</h1>} />
+              <Route path="Assignments" element={<Assignments />} />
+              <Route path="Assignments/:id" element={<AssignmentEditor />} />
+              <Route path="Quizzes" element={<h1>Quizzes</h1>} />
+              <Route path="Grades" element={<Grades />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
-
