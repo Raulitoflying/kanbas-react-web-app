e41efbace8f0fbeffab3c20f92e5b44d917f8aa0
Raulitoflying <1017329098@qq.com>
2024-07-23 22:35:54 -0700

    Refactor ModulesControls component

--- src/Kanbas/Courses/Modules/ModulesControls.tsx
+++ src/Kanbas/Courses/Modules/ModulesControls.tsx
@@ -1,17 +1,15 @@
+import { FaPlus } from 'react-icons/fa6';
 import React from 'react';
-import { FaPlus } from "react-icons/fa6";
-import GreenCheckmark, { RedCrossMark } from "./GreenCheckmark";
+import GreenCheckmark from './GreenCheckmark';
 
 export default function ModulesControls() {
   return (
     <div id="wd-modules-controls" className="text-nowrap">
       <button id="wd-add-module-btn" className="btn btn-lg btn-danger me-1 float-end">
-        <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
-        Module
+        <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} /> Module
       </button>
       <div className="dropdown d-inline me-1 float-end">
-        <button id="wd-publish-all-btn" className="btn btn-lg btn-secondary dropdown-toggle"
-          type="button" data-bs-toggle="dropdown">
+        <button id="wd-publish-all-btn" className="btn btn-lg btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
           <GreenCheckmark />
           Publish All
         </button>
@@ -30,24 +28,20 @@ export default function ModulesControls() {
           </li>
           <li>
             <a id="wd-unpublish-all-modules-and-items" className="dropdown-item" href="#">
-              <RedCrossMark />
+              <GreenCheckmark />
               Unpublish all modules and items
             </a>
           </li>
           <li>
             <a id="wd-unpublish-modules-only" className="dropdown-item" href="#">
-              <RedCrossMark />
+              <GreenCheckmark />
               Unpublish modules only
             </a>
           </li>
         </ul>
       </div>
-      <button id="wd-view-progress" className="btn btn-lg btn-info me-1 float-end">
-        View Progress
-      </button>
-      <button id="wd-collapse-all" className="btn btn-lg btn-warning me-1 float-end">
-        Collapse All
-      </button>
+      <button id="wd-view-progress" className="btn btn-lg btn-secondary me-1">View Progress</button>
+      <button id="wd-collapse-all" className="btn btn-lg btn-secondary">Collapse ALL</button>
     </div>
   );
 }
