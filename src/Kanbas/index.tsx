80499bcc71d05c3b1332cfa679e621f5dd9aff46
Raulitoflying <1017329098@qq.com>
2024-07-23 22:32:52 -0700

    Update react-router-dom version

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -4,24 +4,16 @@ import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
 import "./styles.css";
 
-
 export default function Kanbas() {
   return (
     <div id="wd-kanbas">
-      <h1>Yixiang Zhou/Raul</h1>
-      <table border={1} width="100%">
-        <tr>
-          <td valign="top">
-            <KanbasNavigation />
-          </td>
-          <td valign="top">
-            <Routes>
-              <Route path="/Courses/:id/*" element={<Courses />} />
-              <Route path="/Dashboard" element={<Dashboard />} />
-            </Routes>
-          </td>
-        </tr>
-      </table>
+      <KanbasNavigation />
+      <div className="wd-main-content-offset p-3">
+        <Routes>
+          <Route path="Dashboard" element={<Dashboard />} />
+          <Route path="Courses/*" element={<Courses />} />
+        </Routes>
+      </div>
     </div>
   );
 }
