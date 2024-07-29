3bab822b3708f072b8c90edd808165e46a27e1af
Raulitoflying <1017329098@qq.com>
2024-07-28 17:29:27 -0700

    Add Lab4 component and update GitHub link

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,14 +1,21 @@
+///Users/sophiawang/2024/summer/webdev/kanbas-react-web-app/src/Labs/index.tsx
+
 import React from 'react';
 import { Route, Routes, Navigate } from 'react-router';
 import Lab1 from './Lab1';
 import Lab2 from './Lab2';
 import Lab3 from './Lab3';
+import Lab4 from './Lab4';
+import LandingPage from '../LandingPage';
 import TOC from './TOC';
+import store from './store';
+import { Provider } from 'react-redux';
 
 const Labs: React.FC = () => {
   return (
+    <Provider store={store}>
     <div>
-      <h1>Yixiang Zhou/Raul</h1>
+      <h1>Yixiang ZHou/Raul</h1>
       <h1>Labs</h1>
       <TOC />
       <Routes>
@@ -16,11 +23,14 @@ const Labs: React.FC = () => {
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2" element={<Lab2 />} />
         <Route path="Lab3/*" element={<Lab3 />} />
+        <Route path="Lab4/*" element={<Lab4 />} />
+        <Route path="*" element={<LandingPage />} />
       </Routes>
-      <a href="https://github.com/Raulitoflying/kanbas-react-web-app/tree/a1/">Github</a>
+      <a href="https://github.com/thisissophiawang/kanbas-react-web-app/tree/a1">Github</a>
     </div>
+    </Provider>
+
   );
 }
 
 export default Labs;
-
