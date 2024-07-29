2b072851951ef0e5bb596fb15d832e1a8bec3541
Raulitoflying <1017329098@qq.com>
2024-07-28 17:29:19 -0700

    Add LandingPage to KanbasNavigation

--- src/Kanbas/Navigation/index.tsx
+++ src/Kanbas/Navigation/index.tsx
@@ -12,6 +12,8 @@ export default function KanbasNavigation() {
     { label: "Calendar",  path: "/Kanbas/Calendar",  icon: IoCalendarOutline },
     { label: "Inbox",     path: "/Kanbas/Inbox",     icon: FaInbox },
     { label: "Labs",      path: "/Labs",             icon: LiaCogSolid },
+    // To LandinPage
+    { label: "LandingPage", path: "/LandingPage",    icon: LiaCogSolid },
   ];
   return (
     <div id="wd-kanbas-navigation" className="list-group rounded-0">
