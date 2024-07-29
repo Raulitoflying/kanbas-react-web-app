71c356cce1ceeffe0d60f929709b2fb6c7d6cd5a
Raulitoflying <1017329098@qq.com>
2024-07-29 04:17:53 -0700

    Update ModuleControlButtons component

--- src/Kanbas/Courses/Modules/ModuleControlButtons.tsx
+++ src/Kanbas/Courses/Modules/ModuleControlButtons.tsx
@@ -1,10 +1,20 @@
-import { IoEllipsisVertical } from "react-icons/io5";
-import { BsPlus } from "react-icons/bs"; // import the BsPlus icon
-import GreenCheckmark from "./GreenCheckmark";
+// src/Kanbas/Courses/Modules/ModuleControlButtons.tsx
+import { FaTrash, FaPencilAlt } from 'react-icons/fa';
+import { IoEllipsisVertical } from 'react-icons/io5';
+import { BsPlus } from 'react-icons/bs';
+import GreenCheckmark from './GreenCheckmark';
 
-export default function ModuleControlButtons() {
+interface ModuleControlButtonsProps {
+  moduleId: string;
+  deleteModule: (moduleId: string) => void;
+  editModule: (moduleId: string) => void;
+}
+
+export default function ModuleControlButtons({ moduleId, deleteModule, editModule }: ModuleControlButtonsProps) {
   return (
     <div className="float-end">
+      <FaPencilAlt onClick={() => editModule(moduleId)} className="text-primary me-3" />
+      <FaTrash className="text-danger me-2 fs-4" onClick={() => deleteModule(moduleId)} />
       <GreenCheckmark />
       <BsPlus className="fs-4" />
       <IoEllipsisVertical className="fs-4" />
