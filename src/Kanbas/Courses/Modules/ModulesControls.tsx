73ca3ec3084b998b9f2c6d14347399a851a5b1cc
Raulitoflying <1017329098@qq.com>
2024-07-29 04:18:14 -0700

    Refactor ModulesControls component to add ModuleEditor

--- src/Kanbas/Courses/Modules/ModulesControls.tsx
+++ src/Kanbas/Courses/Modules/ModulesControls.tsx
@@ -1,15 +1,33 @@
-import { FaPlus } from 'react-icons/fa6';
+// src/Kanbas/Courses/Modules/ModulesControls.tsx
 import React from 'react';
+import { FaPlus } from 'react-icons/fa';
 import GreenCheckmark from './GreenCheckmark';
+import ModuleEditor from './ModuleEditor';
 
-export default function ModulesControls() {
+interface ModulesControlsProps {
+  setModuleName: (name: string) => void;
+  moduleName: string;
+  addModule: () => void;
+}
+
+export default function ModulesControls({ setModuleName, moduleName, addModule }: ModulesControlsProps) {
   return (
     <div id="wd-modules-controls" className="text-nowrap">
-      <button id="wd-add-module-btn" className="btn btn-lg btn-danger me-1 float-end">
+      <button
+        id="wd-add-module-btn"
+        className="btn btn-lg btn-danger me-1 float-end"
+        data-bs-toggle="modal"
+        data-bs-target="#wd-add-module-dialog"
+      >
         <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} /> Module
       </button>
       <div className="dropdown d-inline me-1 float-end">
-        <button id="wd-publish-all-btn" className="btn btn-lg btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+        <button
+          id="wd-publish-all-btn"
+          className="btn btn-lg btn-secondary dropdown-toggle"
+          type="button"
+          data-bs-toggle="dropdown"
+        >
           <GreenCheckmark />
           Publish All
         </button>
@@ -42,6 +60,12 @@ export default function ModulesControls() {
       </div>
       <button id="wd-view-progress" className="btn btn-lg btn-secondary me-1">View Progress</button>
       <button id="wd-collapse-all" className="btn btn-lg btn-secondary">Collapse ALL</button>
+      <ModuleEditor
+        dialogTitle="Add Module"
+        moduleName={moduleName}
+        setModuleName={setModuleName}
+        addModule={addModule}
+      />
     </div>
   );
 }
