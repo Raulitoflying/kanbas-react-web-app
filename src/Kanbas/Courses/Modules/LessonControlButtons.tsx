613d2725fae26f8c74cb8ab691903961146986a6
Raulitoflying <1017329098@qq.com>
2024-08-05 14:04:42 -0700

    Refactor LessonControlButtons component

--- src/Kanbas/Courses/Modules/LessonControlButtons.tsx
+++ src/Kanbas/Courses/Modules/LessonControlButtons.tsx
@@ -1,11 +1,10 @@
-import { BsPlus } from 'react-icons/bs';
-import GreenCheckmark from './GreenCheckmark';
-
-export default function ModuleControlButtons() {
-  return (
-    <div className="float-end">
-      <GreenCheckmark />
-      <BsPlus className="fs-4" />
-    </div>
-  );
+import { IoEllipsisVertical } from "react-icons/io5";
+import GreenCheckmark from "./GreenCheckmark";
+export default function LessonControlButtons() {
+    return (
+        <div className="float-end">
+            <GreenCheckmark />
+            <IoEllipsisVertical className="fs-4" />
+        </div>
+    );
 }
