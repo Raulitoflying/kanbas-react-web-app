ddba3b5914a425585bed7330e0a6d2e088b8b43e
Raulitoflying <1017329098@qq.com>
2024-08-05 14:03:54 -0700

    Refactor imports in store.ts

--- src/Kanbas/store.ts
+++ src/Kanbas/store.ts
@@ -1,13 +1,10 @@
-// src/Kanbas/store.ts
-import { configureStore } from '@reduxjs/toolkit';
-import modulesReducer from './Courses/Modules/reducer';
-import assignmentsReducer from './Courses/Assignments/reducer'; // Import the assignments reducer
-
+import { configureStore } from "@reduxjs/toolkit";
+import modulesReducer from "./Courses/Modules/reducer";
+import assignmentsReducer from "./Courses/Assignments/reducer";
 const store = configureStore({
   reducer: {
-    modules: modulesReducer,
-    assignments: assignmentsReducer, // Add the assignments reducer
+    modulesReducer,
+    assignmentsReducer,
   },
 });
-
 export default store;
