efaf19fac9914e56902e99a22ab9ec031e487c72
Raulitoflying <1017329098@qq.com>
2024-08-13 16:24:57 -0700

    Refactor routes in Account component

--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -1,9 +1,9 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Signin from "./Signin";
-import { useSelector } from "react-redux";
 import AccountNavigation from "./Navigation";
 import Profile from "./Profile";
 import Signup from "./Signup";
+import { useSelector } from "react-redux";
 export default function Account() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   return (
@@ -14,18 +14,7 @@ export default function Account() {
         </div>
         <div className="flex-fill p-4 pt-0">
           <Routes>
-            <Route
-              path="/"
-              element={
-                <Navigate
-                  to={
-                    currentUser
-                      ? "/Kanbas/Account/Profile"
-                      : "/Kanbas/Account/Signin"
-                  }
-                />
-              }
-            />
+            <Route path="/" element={ <Navigate to={ currentUser ? "/Kanbas/Account/Profile" : "/Kanbas/Account/Signin" }/>} />
             <Route path="/Signin" element={<Signin />} />
             <Route path="/Signup" element={<Signup />} />
             <Route path="/Profile" element={<Profile />} />
