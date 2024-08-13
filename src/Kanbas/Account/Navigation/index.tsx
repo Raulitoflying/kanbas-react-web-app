8d3e8a81b7f6bb49169a28040d61f88ad77fe792
Raulitoflying <1017329098@qq.com>
2024-08-13 16:25:23 -0700

    Refactor account navigation component

--- src/Kanbas/Account/Navigation/index.tsx
+++ src/Kanbas/Account/Navigation/index.tsx
@@ -1,6 +1,5 @@
-import { Link, useLocation, useParams } from "react-router-dom";
-import "./index.css";
 import { useSelector } from "react-redux";
+import { Link, useLocation, useParams } from "react-router-dom";
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
@@ -8,13 +7,8 @@ export default function AccountNavigation() {
   return (
     <div id="wd-account-navigation" className="list-group fs-5 rounded-0">
       {links.map((link) => (
-        <Link
-          to={`/Kanbas/Account/${link}`}
-          className={`wd-link border border-0 list-group-item
-           ${pathname.includes(link) ? "active" : "text-danger"}`}
-        >
-          {link}
-        </Link>
+        <Link to={`/Kanbas/Account/${link}`} className={`wd-link border border-0 list-group-item
+           ${ pathname.includes(link) ? "active text-black" : "text-danger" }`}> {link} </Link>
       ))}
     </div>
   );
