d33c01d273210126bb50b0159f75813747b1ee6b
Raulitoflying <1017329098@qq.com>
2024-08-13 16:25:29 -0700

    Refactor course-related functions in client.ts

--- src/Kanbas/Courses/client.ts
+++ src/Kanbas/Courses/client.ts
@@ -1,23 +1,30 @@
 import axios from "axios";
+
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 const COURSES_API = `${REMOTE_SERVER}/api/courses`;
 
-export const updateCourse = async (course: any) => {
-    const response = await axios.put(`${COURSES_API}/${course._id}`, course);
-    return response.data;
-};
-
-export const deleteCourse = async (id: string) => {
-    const response = await axios.delete(`${COURSES_API}/${id}`);
-    return response.data;
-};
-
 export const fetchAllCourses = async () => {
-    const { data } = await axios.get(COURSES_API);
-    return data;
+  const { data } = await axios.get(COURSES_API);
+  return data;
 };
-
 export const createCourse = async (course: any) => {
-    const response = await axios.post(COURSES_API, course);
-    return response.data;
+  try{
+    const response = await axios.post(COURSES_API, course, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  return response.data;
+  }catch(error){
+    console.error('Error creating course:', error);
+    throw error;
+  }
+};
+export const deleteCourse = async (id: string) => {
+  const response = await axios.delete(`${COURSES_API}/${id}`);
+  return response.data;
+};
+export const updateCourse = async (course: any) => {
+  const response = await axios.put(`${COURSES_API}/${course._id}`, course);
+  return response.data;
 };
