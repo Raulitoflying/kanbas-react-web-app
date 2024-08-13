e44295245b1a8789f8c150a218ea037ba11e8d3c
Raulitoflying <1017329098@qq.com>
2024-08-13 16:25:02 -0700

    Refactor Profile component and fix formatting issue

--- src/Kanbas/Account/Profile.tsx
+++ src/Kanbas/Account/Profile.tsx
@@ -1,96 +1,63 @@
+import { useEffect, useState } from "react";
 import * as client from "./client";
+import { useNavigate } from "react-router";
 import { useDispatch } from "react-redux";
 import { setCurrentUser } from "./reducer";
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 export default function Profile() {
-  const [profile, setProfile] = useState<any>({});
+  const[profile, setProfile] = useState<any>({});
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const fetchProfile = async () => {
-    try {
+  const fetchProfile = async() => {
+    try{
       const account = await client.profile();
       setProfile(account);
-    } catch (err: any) {
+    }catch(err: any){
       navigate("/Kanbas/Account/Signin");
     }
-  };
+  }
   const signout = async () => {
     await client.signout();
     dispatch(setCurrentUser(null));
     navigate("/Kanbas/Account/Signin");
   };
-  useEffect(() => {
-    fetchProfile();
-  }, []);
-  return (
-    <div id="wd-profile-screen">
+  const formatDate = (isoString: string | number | Date) => {
+    const date = new Date(isoString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  };
+
+
+  useEffect(() => {fetchProfile();}, []);
+  return(
+    <div className="wd-profile-screen">
       <h1>Profile</h1>
       {profile && (
         <div>
-          <input
-            id="wd-username"
-            className="form-control m-2"
-            value={profile.username}
-            onChange={(e) =>
-              setProfile({ ...profile, username: e.target.value })
-            }
-          />
-          <input
-            id="wd-password"
-            className="form-control m-2"
-            value={profile.password}
-            onChange={(e) =>
-              setProfile({ ...profile, password: e.target.value })
-            }
-          />
-          <input
-            id="wd-firstname"
-            className="form-control m-2"
-            value={profile.firstName}
-            onChange={(e) =>
-              setProfile({ ...profile, firstName: e.target.value })
-            }
-          />
-          <input
-            id="wd-lastname"
-            className="form-control m-2"
-            value={profile.lastName}
-            onChange={(e) =>
-              setProfile({ ...profile, lastName: e.target.value })
-            }
-          />
-          <input
-            id="wd-dob"
-            className="form-control m-2"
-            value={profile.dob}
-            onChange={(e) => setProfile({ ...profile, dob: e.target.value })}
-            type="date"
-          />
-          <input
-            id="wd-email"
-            className="form-control m-2"
-            value={profile.email}
-            onChange={(e) => setProfile({ ...profile, email: e.target.value })}
-          />
-          <select
-            id="wd-role"
-            className="form-select m-2"
-            onChange={(e) => setProfile({ ...profile, role: e.target.value })}
-          >
-            <option value="USER">User</option>{" "}
-            <option value="ADMIN">Admin</option>
-            <option value="FACULTY">Faculty</option>{" "}
-            <option value="STUDENT">Student</option>
+          <input className="wd-usrename form-control mb-2" value={profile.username}
+            onChange={(e) => setProfile({ ...profile, usename: e.target.value})}/>
+          <input className="wd-password form-control mb-2" value={profile.password}
+            onChange={(e) => setProfile({...profile, password: e.target.value})}/>
+          <input className="wd-firstname form-control mb-2" value={profile.firstName}
+          onChange={(e) => setProfile({...profile, firstname: e.target.value})}/>
+          <input className="wd-lastname form-control mb-2" value={profile.lastName}
+          onChange={(e) => setProfile({...profile, lastname: e.target.value})}/>
+          <input className="wd-dob form-control mb-2" value={formatDate(profile.dob)}
+                 onChange={(e) => setProfile({ ...profile, dob: e.target.value })} type="date"/>
+          <input className="wd-email form-control mb-2" value={profile.email}
+          onChange={(e) => setProfile({...profile, email: e.target.value})}  />
+          <br />
+          <select className="wd-role form-control mb-2" onChange={(e) => setProfile({...profile, role: e.target.value})}>
+            <option value="USER">User</option>   <option value="ADMIN">Admin</option>
+            <option value="FACULTY">Faculty</option>   <option value="STUDENT">Student</option>
           </select>
-          <button
-            onClick={signout}
-            className="wd-signout-btn btn btn-danger w-100"
-          >
+          <button onClick={signout} className="wd-signout-btn btn btn-danger w-100">
             Sign out
           </button>
         </div>
       )}
+
     </div>
-  );
+  )
 }
